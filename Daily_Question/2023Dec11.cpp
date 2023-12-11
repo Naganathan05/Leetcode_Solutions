6ba1683ff7058f9cb6dfbd022c1b52e85bb0c243
Naganathan <116747452+Naganathan05@users.noreply.github.com>
2023-12-11 18:52:43 +0530

    Update 2023Dec11.cpp

--- Daily_Question/2023Dec11.cpp
+++ Daily_Question/2023Dec11.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int cnt = 1;
+        for(int i = 0; i <= arr.size() - 1; i++){
+            if(arr[i] == arr[i + arr.size()/4]){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+};
