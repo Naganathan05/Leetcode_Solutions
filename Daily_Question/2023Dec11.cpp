aabb2af53cf1389bb6476e52b0ffd0213f28c48f
Naganathan <116747452+Naganathan05@users.noreply.github.com>
2023-12-12 10:21:03 +0530

    Update 2023Dec11.cpp

--- Daily_Question/2023Dec11.cpp
+++ Daily_Question/2023Dec11.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
-        int cnt = 1;
         for(int i = 0; i <= arr.size() - 1; i++){
             if(arr[i] == arr[i + arr.size()/4]){
                 return arr[i];
@@ -10,3 +9,6 @@ public:
         return -1;
     }
 };
+
+
+// Question Link: https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/description/?envType=daily-question&envId=2023-12-11
