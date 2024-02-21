4201a95bac13200691a562157edbda6e38ff0e56
Naganathan <umaiyalramesh@gmail.com>
2024-02-21 10:44:26 +0530

    Problem Difficulty Level: Easy

--- Dynamic Programming/70. Climbing Stairs.cpp
+++ Dynamic Programming/70. Climbing Stairs.cpp
@@ -1,4 +1,5 @@
 /*----------------------------------------------
+Approach 1: Memoization
   Time Complexity: O(n)
   Space Complexity: O(n)
 -----------------------------------------------*/
@@ -24,6 +25,31 @@ public:
         }
         
         dp[n] = recursiveClimb(n - 1, dp) + recursiveClimb(n - 2, dp);
+        return dp[n];
+    }
+};
+
+
+
+/*---------------------------------------------------------------
+Approach 2: Tabulation
+  Time Complexity: O(n)
+  Space Complexity: O(n)
+-----------------------------------------------------------------*/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1);
+        for(int i = 0; i <= n; i++){
+            dp[i] = -1;
+        }
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
         return dp[n];
     }
 };
