13a7a66ac8b1364584d598a42929ed0d5c71ea9c
Naganathan <umaiyalramesh@gmail.com>
2024-07-14 09:37:24 +0530

    Problem Difficulty Level: Ea

---
+++ Strings/3216. Lexographically Smallest String after a Swap.cpp
@@ -0,0 +1,24 @@
+/*----------------------------
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+------------------------------*/
+
+class Solution {
+public:
+    string getSmallestString(string s) {
+        for(int i = 0; i < s.length() - 1; i++){
+            if ((s[i] - '0') % 2 == (s[i + 1] - '0') % 2){
+                if(s[i] > s[i + 1]){
+                    swap(s[i], s[i + 1]);
+                    return s;
+                }
+            }
+        }
+        return s;
+    }
+};
+
+/*
+Question Link: https://leetcode.com/problems/lexicographically-smallest-string-after-a-swap/
+Author: M.R.Naganathan
+*/
