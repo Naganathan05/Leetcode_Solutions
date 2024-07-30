afc273d208ca5a68b2ec20adbfd491060cf867bc
Naganathan <umaiyalramesh@gmail.com>
2024-07-30 19:26:37 +0530

    Problem Difficulty Level: Medium

---
+++ Greedy Algorithms/55. Jump Game.cpp
@@ -0,0 +1,21 @@
+/*----------------------------
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+------------------------------*/
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+        for(int i = 0; i <= nums.size() - 1; i++){
+            if(i > maxReach) return false;
+            maxReach = max(maxReach, i + nums[i]);
+        }
+        return true;
+    }
+};
+
+/*
+Question Link: https://leetcode.com/problems/jump-game/
+Author: M.R.Naganathan
+*/
