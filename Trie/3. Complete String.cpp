a3c52293b750bb6d332eae9cebff69edd8137a6c
Naganathan <umaiyalramesh@gmail.com>
2024-09-27 12:50:08 +0530

    Problem Difficulty Level: Hard

---
+++ Trie/3. Complete String.cpp
@@ -0,0 +1,94 @@
+/*-------------------------------
+  Time Complexity: O(n * maxLen)
+  Space Complexity: O(maxLen)
+----------------------------------*/
+
+#include <bits/stdc++.h> 
+
+class Node {
+private:
+    Node* links[26];
+    bool isEnd;
+public:
+    Node() {
+        isEnd = false;
+        for(int i = 0; i <= 25; i++) links[i] = NULL;
+    }
+
+    bool containsLink(char c){
+        return links[c - 'a'] != NULL;
+    }
+
+    void putLink(char c, Node* node){
+        links[c - 'a'] = node;
+        return;
+    }
+
+    Node* getLink(char c){
+        return links[c - 'a'];
+    }
+
+    bool checkEnd(){
+        return isEnd;
+    }
+
+    void setEnd(){
+        isEnd = true;
+        return;
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+public:
+    Trie(){
+        root = new Node();
+    }
+
+    void insertString(string &word){
+        Node* currNode = root;
+        for(int i = 0; i <= word.length() - 1; i++){
+            if(!(currNode -> containsLink(word[i]))) currNode -> putLink(word[i], new Node());
+            currNode = currNode -> getLink(word[i]);
+        }
+        currNode -> setEnd();
+        return;
+    }
+
+    bool searchPrefix(string &prefix){
+        Node* currNode = root;
+        for(int i = 0; i <= prefix.length() - 1; i++){
+            if(!(currNode -> containsLink(prefix[i]))) return false;
+            currNode = currNode -> getLink(prefix[i]);
+            if(!(currNode -> checkEnd())) return false;
+        }
+        return true;
+    }
+};
+
+string completeString(int n, vector<string> &a){
+    Trie* prefixTree = new Trie();
+    for(int i = 0; i <= n - 1; i++){
+        prefixTree -> insertString(a[i]);
+    }
+
+    int maxLen = 0;
+    string maxCompleteString = "None";
+    for(int i = 0; i <= n - 1; i++){
+        if(!(prefixTree -> searchPrefix(a[i]))) continue;
+        if(maxLen < (int)a[i].length()){
+            maxLen = (int)a[i].length();
+            maxCompleteString = a[i];
+        }
+        else if(maxLen == (int)a[i].length() && a[i] < maxCompleteString){
+            maxCompleteString = a[i];
+        }
+    }
+    return maxCompleteString;
+}
+
+/*
+Question Link: https://www.naukri.com/code360/problems/complete-string_2687860/
+Author: M.R.Naganathan
+*/
