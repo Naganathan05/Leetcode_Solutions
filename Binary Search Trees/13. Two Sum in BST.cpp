c05dc4aa7ed612749b33e9a76c6dda4e0ed09a1c
Naganathan <umaiyalramesh@gmail.com>
2024-11-30 11:47:12 +0530

    Problem Difficulty Level: Easy

--- Binary Search Trees/13. Two Sum in BST.cpp
+++ Binary Search Trees/13. Two Sum in BST.cpp
@@ -23,7 +23,34 @@ bool twoSumInBST(BinaryTreeNode<int>* root, int target) {
     return false;
 }
 
+/*-------------------------------
+   Time Complexity: O(nlogn)
+   Space Complexity: O(h)
+---------------------------------*/
+
+class Solution {
+public:
+    bool binarySearch(TreeNode* root, int &target, TreeNode* node){
+        if(!root) return false;
+        if(root -> val == target && root != node) return true;
+        if(root -> val > target) return binarySearch(root -> left, target, node);
+        return binarySearch(root -> right, target, node);
+    }
+
+    bool DFS(TreeNode* currNode, TreeNode* root, int k){
+        if(!currNode) return false;
+        int target = k - (currNode -> val);
+        if(binarySearch(root, target, currNode) || binarySearch(root, target, currNode)) return true;
+        return DFS(currNode -> left, root, k) || DFS(currNode -> right, root, k);
+    }
+
+    bool findTarget(TreeNode* root, int k) {
+        return DFS(root, root, k);
+    }
+};
+
 /*
-Question Link: https://www.naukri.com/code360/problems/pair-sum_4444818
+Question Link [Coding Ninja]: https://www.naukri.com/code360/problems/pair-sum_4444818
+Question Link [Leetcode]: https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
 Author: M.R.Naganathan
 */
