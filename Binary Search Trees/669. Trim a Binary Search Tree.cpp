5ef87460a4a3391f0dd6ce4dad7efa7e6f8b1223
Naganathan <umaiyalramesh@gmail.com>
2024-12-03 08:47:42 +0530

    Problem Difficulty Level: Medium

---
+++ Binary Search Trees/669. Trim a Binary Search Tree.cpp
@@ -0,0 +1,21 @@
+/*------------------------
+  Time Complexity: O(n)
+  Space Complexity: O(h)
+--------------------------*/
+
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(!root) return NULL;
+        if(root -> val < low) return trimBST(root -> right, low, high);
+        if(root -> val > high) return trimBST(root -> left, low, high);
+        root -> left = trimBST(root -> left, low, high);
+        root -> right = trimBST(root -> right, low, high);
+        return root;
+    }
+};
+
+/*
+Question Link: https://leetcode.com/problems/trim-a-binary-search-tree/
+Author: M.R.Naganathan
+*/
