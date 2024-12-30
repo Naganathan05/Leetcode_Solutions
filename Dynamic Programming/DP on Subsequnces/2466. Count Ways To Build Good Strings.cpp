106c4fc7b9f0d5f7b955af0c06abdac89347a536
Naganathan <umaiyalramesh@gmail.com>
2024-12-31 00:02:09 +0530

    Problem Difficulty Level: Medium

---
+++ Dynamic Programming/DP on Subsequnces/2466. Count Ways To Build Good Strings.cpp
@@ -0,0 +1,29 @@
+/*---------------------------
+  Time Complexity: O(n)
+  Space Complexity: O(n)
+-----------------------------*/
+
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    long long  helper(int length, int &high, vector<long long>& dp, int &low, int &zero, int &one) {
+        if(length > high) return 0;
+
+        if(dp[length] != -1) return dp[length];
+        
+        dp[length] = length >= low ? 1 : 0;
+
+        dp[length] += helper(length + zero, high, dp, low, zero, one) + helper(length + one, high, dp, low, zero, one);
+        return dp[length] % mod;
+    }
+
+    int countGoodStrings(int low, int high, int zero, int one) {
+        vector<long long> dp(high + 1, -1);
+        return helper(0, high, dp, low, zero, one);
+    }
+};
+
+/*
+Question Link: https://leetcode.com/problems/count-ways-to-build-good-strings/
+Author: M.R.Naganathan
+*/
