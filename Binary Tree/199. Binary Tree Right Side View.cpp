ae2ab957001c9c045225e84d348c51e1823ae02b
Naganathan <naganathan1555@gmail.com>
2025-01-18 09:49:28 +0530

    Problem Difficulty Level: Medium

--- Binary Tree/199. Binary Tree Right Side View.cpp
+++ Binary Tree/199. Binary Tree Right Side View.cpp
@@ -27,6 +27,34 @@ public:
     }
 };
 
+/*---------------------------
+Method: Depth First Search
+  Time Complexity: O(n)
+  Space Complexity: O(n)
+------------------------------*/
+
+class Solution {
+public:
+    void DFS(TreeNode* root, int currHeight, map<int, int> &rightView){
+        if(!root) return;
+        
+        if(rightView.find(currHeight) == rightView.end()){
+            rightView[currHeight] = root -> val;
+        }
+        DFS(root -> right, currHeight + 1, rightView);
+        DFS(root -> left, currHeight + 1, rightView);
+        return;
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        map<int, int> rightView;
+        DFS(root, 0, rightView);
+        vector<int> ans;
+        for(auto i : rightView) ans.push_back(i.second);
+        return ans;
+    }
+};
+
 /*
 Question Link: https://leetcode.com/problems/binary-tree-right-side-view/
 Author: M.R.Naganathan
