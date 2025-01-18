e33f7ef1a51337fed672f9b88871f1b4b9fb4b92
Naganathan <naganathan1555@gmail.com>
2025-01-18 11:42:47 +0530

    Problem Difficulty Level: Hard

--- Graphs/Shortest Path/2290. Minimum Obstacle Removal to Reach Coner.cpp
+++ Graphs/Shortest Path/2290. Minimum Obstacle Removal to Reach Coner.cpp
@@ -42,6 +42,49 @@ public:
     }
 };
 
+/*---------------------------------
+Method: 0-1 Breadth First Search
+  Time Complexity: O(m * n)
+  Space Complexity: O(m * n)
+-----------------------------------*/
+
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dist(n, vector<int> (m, 1e9));
+        deque<pair<int, int>> deque;
+        deque.push_front({0, 0});
+        dist[0][0] = 0;
+
+        while(!deque.empty()){
+            auto [currX, currY] = deque.front();
+            deque.pop_front();
+
+            vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+            for(auto dir : directions){
+                int newX = currX + dir.first;
+                int newY = currY + dir.second;
+
+                if(newX >= 0 && newY >= 0 && newX < n && newY < m){
+                    if(dist[newX][newY] > dist[currX][currY] + grid[newX][newY]){
+                        dist[newX][newY] = dist[currX][currY] + grid[newX][newY];
+                        if(grid[newX][newY] == 1) deque.push_back({newX, newY});
+                        else deque.push_front({newX, newY});
+                    }
+                }
+            }
+        }
+        return dist[n - 1][m - 1];
+    }
+};
+
 /*
 Question Link: https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/
 Author: M.R.Naganathan
